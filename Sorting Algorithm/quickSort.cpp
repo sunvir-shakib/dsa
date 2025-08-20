c74640046f482ab62a436d0bf8a41e1cf0519111
Sunvir Shakib <Sunvir Shakib>
2025-08-21 02:21:04 +0600

    qk

--- Sorting Algorithm/quickSort.cpp
+++ Sorting Algorithm/quickSort.cpp
@@ -1,14 +1,68 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printArr(int *arr, int n){
-    for(int i=0; i<n; i++)
-    cout<<arr[i]<<" ";
+//pivot at first idx
+int partition(vector<int>& ar, int si, int ei) {
+    int pivot = ar[si];  
+    int i = si;
 
-    cout<<endl;
+    for (int j = si + 1; j <= ei; j++) {
+        if (ar[j] < pivot) {  
+            i++;
+            swap(ar[i], ar[j]);
+        }
+    }
+
+    swap(ar[i], ar[si]);  
+    return i;
 }
 
-int partition(int *arr, int si, int ei){
+
+//pivot at mid idx
+int partition(vector<int> &ar, int si, int ei){
+    int mid = (si+ei)/2; //pivot idx at mid
+    int pivot = ar[mid];
+    // swap(pivot, ar[ei]); wrong bcz pivot is a local variable which is not working at swap fun.
+    swap(ar[mid], ar[ei]);
+    int i = si-1;
+
+    for(int j=si; j<ei; j++){
+        if(ar[j]<=pivot){
+            i++;
+            swap(ar[i], ar[j]);
+        }    
+    }
+    i++;
+    swap(ar[i], ar[ei]);
+    return i;
+}
+
+//pivot at mid diff types
+int partition2(vector<int> &ar, int si, int ei){
+    int pivot = ar[(si+ei)/2]; // pivot at mid
+    int i = si;
+    int j = ei;
+    
+    while(i <= j){
+        while(ar[i] < pivot){
+            i++;
+        }
+        while(ar[j] > pivot){
+            j--;
+        }
+        
+        // If we found elements to swap
+        if(i <= j){
+            swap(ar[i], ar[j]);
+            i++;
+            j--;
+        }
+    }
+    return i; 
+}
+
+//pivot at end idx
+int partition(vector<int> &arr, int si, int ei){
     int i = si-1;
     int pivot = arr[ei];
     
@@ -20,27 +74,34 @@ int partition(int *arr, int si, int ei){
     }
     i++;
     swap(arr[i], arr[ei]);
-//pivot[idx]=i
     return i; //return pivot idx
 }
 
-void quickSort(int *arr, int si, int ei){
-    if(si>=ei) //base case
+void quickSort(vector<int> &ar, int si, int ei){
+    if(si>=ei)
     return;
 
-    int pivotIDX = partition(arr, si, ei);
-
-    quickSort(arr, si, pivotIDX-1); //left half
-    quickSort(arr, pivotIDX+1, ei); //right half
-
+    int pivotIDX = partition(ar, si, ei);
+    quickSort(ar, si, pivotIDX-1); 
+    quickSort(ar, pivotIDX+1, ei); 
 }
+    
 
 int main() 
 {
-    int n = 6;
-    int arr[6]={6, 3, 7, 5, 2, 4};
-    quickSort(arr, 0, n-1);
-    printArr(arr,n);
+   int n;
+   cin>>n;
+   vector<int> ar(n);
+   for(int i=0; i<n; i++){
+    cin>>ar[i];
+   }
+
+   quickSort(ar, 0, n-1);
    
+   for(int i=0; i<n; i++){
+        cout<<ar[i]<<" ";
+    }
+    cout<<endl;
+
     return 0;
 }
