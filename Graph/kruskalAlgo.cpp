ee274348a0d495bb2e0b06ef1e29d5b71542d510
Sunvir Shakib <sunvirshakib@gmail.com>
2025-09-22 15:55:04 +0600

    kr

--- Graph/kruskalAlgo.cpp
+++ Graph/kruskalAlgo.cpp
@@ -1,88 +1,86 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
+#include<algorithm> //for sort fun
 using namespace std;
-
-class Edge{
-public:
-    int u, v, wt;
-    Edge(int u, int v, int wt){
-        this->u = u;
-        this->v = v;
-        this->wt = wt;
-    }
-};
 
 class Graph{
 public:
-    vector<Edge> edges;
     int V;
+    vector<pair<int, pair<int,int>>> edges; //{w, u, v}
     vector<int> par;
     vector<int> rank;
-
-    Graph(int V){ 
-        this-> V = V; 
-        for(int i=0; i<V; i++){ 
-            par.push_back(i); 
-            rank.push_back(0); 
+    
+    Graph(int V){
+        this->V = V;
+        par.resize(V);
+        rank.resize(V);
+        
+        for(int i=0; i<V; i++){
+            par[i] = i;
+            rank[i] = 0;
         }
     }
-
+    
     int find(int x){
-        if(par[x] == x){
-        return x;
-        } 
-        return par[x] = find(par[x]); // path compression
+        if(par[x] == x) return x;
+        return par[x] = find(par[x]); // Path compression
     }
+    
+    void unionByRank(int parU, int parV){
+        
+        if(rank[parU] > rank[parV]){
+            par[parV] = parU;
+        }
 
-    void unionByRank(int u, int v){
-        if(rank[u] == rank[v]){
-            par[v] = u;
-            rank[u]++;
+        else if(rank[parU] < rank[parV]){
+            par[parU] = parV;
         }
-        else if(rank[u] > rank[v]){
-            par[v] = u;
-        }
+
         else{
-            par[u] = v;
+            par[parV] = parU;
+            rank[parU]++;
         }
     }
-
-    void addEdge(int u, int v, int wt){
-        edges.push_back(Edge(u,v,wt));
-    }
-
-    void kruskals(){
-        sort(edges.begin(), edges.end(), [](Edge &a, Edge &b){return a.wt < b.wt;});
-        int minCost = 0, count = 0;
-
-        for(auto &e : edges){
-            int parU = find(e.u);
-            int parV = find(e.v);
-
+    
+    void kruskal(){
+        sort(edges.begin(), edges.end()); // 3rd: Sort by weight
+        int minCost = 0;
+        
+        for(int i=0; i<edges.size(); i++){
+            int w = edges[i].first;
+            int u = edges[i].second.first;
+            int v = edges[i].second.second;
+            
+            int parU = find(u); // 4th: Find parents
+            int parV = find(v);
+            
             if(parU != parV){
-                unionByRank(parU, parV);
-                minCost += e.wt;
-                count++;
-                if(count == V-1) 
-                break;
+                unionByRank(parU, parV); // 5th: Union
+                minCost += w;
             }
         }
-
-        cout << "Min cost: " << minCost << endl;
+        cout<<minCost<<endl;
+    }
+    
+    void addEdge(int w, int u, int v){
+        edges.push_back({w, {u,v}});
     }
 };
 
-int main(){
-    Graph graph(5);
-
-    graph.addEdge(0,1,10);
-    graph.addEdge(0,2,15);
-    graph.addEdge(0,3,30);
-    graph.addEdge(1,3,40);
-    graph.addEdge(2,3,50);
-
-    graph.kruskals();
-
+int main()
+{
+    int V,E;
+    cin>>V>>E;
+    
+    Graph g(V);
+    
+    for(int i=0; i<E; i++){
+        int w, u, v;
+        cin>>w>>u>>v;
+        g.addEdge(w, u, v); // 1st: Add edges
+    }
+    
+    g.kruskal(); // 2nd: Run algorithm
+    
     return 0;
 }
